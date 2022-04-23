685fa3b4a3fafdf973a4dcdccbbf44ae8eeb9d7d
stefanl <stefanlinde1995@gmail.com>
2022-04-23 15:36:02 +0300

    map

--- src/components/ShipmentCard.js
+++ src/components/ShipmentCard.js
@@ -15,26 +15,33 @@ const ShipmentCard = () => {
 
   const { id } = useParams();
   const found = data.some((el) => el.id === id);
+  const selectedDataItem = data.filter((item) => item.id === id);
 
   return (
     <div id="shipment-card">
-      {data
-        .filter((item) => item.id === id)
-        .map((validItem) => (
+      {selectedDataItem.map((item) => {
+        const { boxes, name, email } = item;
+        const boxesToInt = boxes.split(",").map((n) => Number(n));
+        const reducer = (accumulator, curr) => accumulator + curr;
+        const numberOfBays = Math.round(boxesToInt.reduce(reducer) / 10);
+        return (
           <div key={id} className="details-wrapper">
-            <h2 className="text-white title">{validItem.name}</h2>
-            <p className="text-grey">{validItem.email}</p>
+            <h2 className="text-white title">{name}</h2>
+            <p className="text-grey">{email}</p>
             <h3 className="text-grey h6 cargo-boxes mb-2">CARGO BOXES</h3>
-            <p className="bg-white rounded d-inline px-4">{validItem.boxes}</p>
+            <p className="bg-white rounded d-inline px-4">{boxes}</p>
             <div className="cargo-bays-wrapper">
               <h3 className="text-grey mt-4">Number of required cargo bays</h3>
-              <p className="cargo-bays-nr h2 text-white">
-                {validItem.numberOfBays}
-              </p>
+              <p className="cargo-bays-nr h2 text-white">{numberOfBays}</p>
             </div>
           </div>
-        ))}
-      {!found && <h2 className="text-white">Unable to process.</h2>}
+        );
+      })}
+      {!found && (
+        <h2 className="text-white">
+          No such cargo in data / unable to process.
+        </h2>
+      )}
     </div>
   );
 };
